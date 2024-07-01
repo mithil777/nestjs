16030d2541a84c94a190aba0b21967cd6c95f45f
itnodejs3 <it.nodejs3@ampersandgroup.in>
2024-07-01 08:49:52 +0530

    first commit

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('ninjas')
+export class NinjasController {
+    // GET /ninjas --> []
+    @Get()
+    getNinjas() {
+        return [];
+    }
+    // GET /ninjas/:id --> { ... }
+    // POST /ninjas
+    // PUT /ninjas/:id --> { ... }
+    // DELETE /ninjas/:id
+}
